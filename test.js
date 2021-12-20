e9c6ce6f776166736c03a725223a8d144f18473a
Ben Hardill <hardillb@gmail.com>
2021-12-20 10:19:11 +0000

    Fix paths

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const nodeRedNodes = require('./node-red-nodes.js')
 got('https://registry.npmjs.org/node-red-node-geofence/-/node-red-node-geofence-0.3.1.tgz').buffer()
 .then( buff => {
 
-    fs.writeFileSync("test/node-red-node-geofence-0.3.1.tgz", buff)
+    fs.writeFileSync("./test/node-red-node-geofence-0.3.1.tgz", buff)
 
     var defs = nodeRedNodes.examinTar("./test/node-red-node-geofence-0.3.1.tgz", "test")
 
@@ -14,7 +14,7 @@ got('https://registry.npmjs.org/node-red-node-geofence/-/node-red-node-geofence-
     fs.rmSync("./test/node-red-node-geofence-0.3.1.tgz")
 })
 .then( () => {
-    var defs = nodeRedNodes.examinModule("./temp/test-node")
+    var defs = nodeRedNodes.examinModule("./test/test-node")
     console.log("\nLocal directory")
     console.log(JSON.stringify(defs,' ',2))
 })
